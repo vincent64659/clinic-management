9fe6db69682c22694d942d5d07f86444148292f4
vincent64659 <vincentpechera0909@gmail.com>
2026-09-23 10:05:56 +0800

    feat: Wroking to controllers check if the code is perfect if not will not to continue to another file

--- services/src/controller/accounts-controller.js
+++ services/src/controller/accounts-controller.js
@@ -4,7 +4,7 @@ const createAccount = async (req, res) => {
     try { 
         const { username, password } = req.body;
         
-        const result = await accountService.createAccount( username, password ); 
+        const result = await accountsService.createAccounts( username, password ); 
         
         return res.status(StatusCodes.CREATED).json({
             success: true, 
@@ -19,11 +19,126 @@ const createAccount = async (req, res) => {
             message: error.message || ReasonPhrases.BAD_REQUEST,
             });
         } 
-    };
+    }; 
+
+const findAllAccountService = require(".. services/accounts-services.js")
+
+const findAllAccount = async (req, res) => {
+    try { 
+        const { username, password } = req.body;
+        
+        const result = await findAllAccountsService.findAllAccountService( username, password ); 
+        
+        return res.status(StatusCodes.CREATED).json({
+            success: true, 
+            message: "Find All Accounts created successfully.",
+            data: result,
+            });
+        } catch (error) {
+        console.error("Find All account error:", error);
+
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            success: false,
+            message: error.message || ReasonPhrases.BAD_REQUEST,
+            });
+        } 
+    }; 
+
+const findAccountsByIdService = require(".. services/accounts-services.js")
+
+const findAccountById = async (req, res) => {
+    try { 
+        const { username, password } = req.body;
+        
+        const result = await findAccountsByIdService.findAccountByIdService( username, password ); 
+        
+        return res.status(StatusCodes.CREATED).json({
+            success: true, 
+            message: "Find Accounts By Id created successfully.",
+            data: result,
+            });
+        } catch (error) {
+        console.error("Find account By Id error:", error);
+
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            success: false,
+            message: error.message || ReasonPhrases.BAD_REQUEST,
+            });
+        } 
+    }; 
+
+const findAccountsByIdService = require(".. services/accounts-services.js")
+
+const findAccountByUsername = async (req, res) => {
+    try { 
+        const { username, password } = req.body;
+        
+        const result = await findAccountsByUsernameService.findAccountByUsernameService( username, password ); 
+        
+        return res.status(StatusCodes.CREATED).json({
+            success: true, 
+            message: "Find Accounts By Username  created successfully.",
+            data: result,
+            });
+        } catch (error) {
+        console.error("Find account By Username error:", error);
+
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            success: false,
+            message: error.message || ReasonPhrases.BAD_REQUEST,
+            });
+        } 
+    }; 
+
+const findAccountsByIdService = require(".. services/accounts-services.js")
+
+const updateAccount = async (req, res) => {
+    try { 
+        const { username, password } = req.body;
+        
+        const result = await findAccountsByUsernameService.updateService( username, password ); 
+        
+        return res.status(StatusCodes.CREATED).json({
+            success: true, 
+            message: "Update accounts created successfully.",
+            data: result,
+            });
+        } catch (error) {
+        console.error("Update accounts error:", error);
+
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            success: false,
+            message: error.message || ReasonPhrases.BAD_REQUEST,
+            });
+        } 
+    }; 
+
+const findAccountsByIdService = require(".. services/accounts-services.js")
+
+const deleteAccount = async (req, res) => {
+    try { 
+        const { username, password } = req.body;
+        
+        const result = await deleteAccountsService.deleteService( username, password ); 
+        
+        return res.status(StatusCodes.CREATED).json({
+            success: true, 
+            message: "Delete accounts created successfully.",
+            data: result,
+            });
+        } catch (error) {
+        console.error("Delete accounts error:", error);
+
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            success: false,
+            message: error.message || ReasonPhrases.BAD_REQUEST,
+            });
+        } 
+    }; 
 
     module.export = {
         createAccount,
-        findAllAccounts,
+        findAllAccount,
         findAccountById,
         findAccountByUsername,
         updateAccount,
